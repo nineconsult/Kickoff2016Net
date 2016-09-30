82dc9fc9da8aa810f8be5320ca40e32fe236c7a1
moanrose <man@nine.dk>
2016-09-30 14:16:51 +0200

    Fixed Not equal to double

--- src/Abp.Web.Resources/Abp/Framework/scripts/abp.js
+++ src/Abp.Web.Resources/Abp/Framework/scripts/abp.js
@@ -86,7 +86,7 @@
     };
 
     abp.auth.isGranted = function (permissionName) {
-        return abp.auth.allPermissions[permissionName] != undefined && abp.auth.grantedPermissions[permissionName] != undefined;
+        return abp.auth.allPermissions[permissionName] !== undefined && abp.auth.grantedPermissions[permissionName] !== undefined;
     };
 
     abp.auth.isAnyGranted = function () {
@@ -278,7 +278,7 @@
             return;
         }
 
-        if (logLevel != undefined && logLevel < abp.log.level) {
+        if (logLevel !== undefined && logLevel < abp.log.level) {
             return;
         }
 
