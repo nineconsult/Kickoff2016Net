0972fc0a19c4eec423aa0bfcafdc752c07ca4b6a
Kenneth Rørstrøm <kro@nine.dk>
2016-09-30 14:28:44 +0200

    dispose

--- src/Abp/Events/Bus/Handlers/IEventHandler.cs
+++ src/Abp/Events/Bus/Handlers/IEventHandler.cs
@@ -1,10 +1,13 @@
+using System;
+
 namespace Abp.Events.Bus.Handlers
 {
     /// <summary>
     ///     Undirect base interface for all event handlers.
     ///     Implement <see cref="IEventHandler{TEventData}" /> instead of this one.
     /// </summary>
-    public interface IEventHandler
+    public interface IEventHandler : IDisposable
     {
+
     }
 }
