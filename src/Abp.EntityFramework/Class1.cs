b35fc7b57e9989d9c6892e42b9dba6b2ee2ba9b4
moanrose <man@nine.dk>
2016-09-30 15:36:37 +0200

    .

---
+++ src/Abp.EntityFramework/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Abp.Domain.Entities;
+using Abp.Events.Bus.Handlers;
+
+namespace Abp
+{
+    class Class1 : Abp.Domain.Entities.IMayHaveTenant, Abp.Domain.Entities.IMustHaveTenant
+    {
+        public int? TenantId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        int IMustHaveTenant.TenantId
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+
+    class Class2 : IEventHandler, IDisposable
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
